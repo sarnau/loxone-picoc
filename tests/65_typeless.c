fc0dee6a6270c84e69dacaee908602bbecc92aff
galacticstudios <galacticstudios@users.noreply.github.com>
2015-08-11 17:21:35 -0400

    Fix errors in the 65_typeless unit test

--- tests/65_typeless.c
+++ tests/65_typeless.c
@@ -2,11 +2,13 @@
 
 void main()
 {
-    x = 3.9;
-    y = 4;
-    z = &y;
-    msg = "hi there";
-    printf("%d %d %d %s\n", x*2, y*2, *z, msg);
+    float x = 3.9;
+    long y = 4;
+    long *z = &y;
+    char *msg = "hi there";
+	int i;
+
+    printf("%d %d %d %s\n", (int) (x*2), y*2, *z, msg);
     printf("%d %d %d\n", sizeof(x), sizeof(y), sizeof(msg));
     for (i = 1; i <= 3; i++)
         printf("%d\n", i);
